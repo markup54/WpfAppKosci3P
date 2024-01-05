02cbfa68c603f8c90af2cb85a66d1d4cb9956576
markup54 <marzenakupka@tlen.pl>
2024-01-05 10:25:19 +0100

    losowanie 5 kości

--- WpfAppKosci3P/MainWindow.xaml.cs
+++ WpfAppKosci3P/MainWindow.xaml.cs
@@ -20,9 +20,36 @@ namespace WpfAppKosci3P
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<BitmapImage> KosciImages { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            przygotujKosci();
+        }
+        private void przygotujKosci()
+        {
+            KosciImages = new List<BitmapImage>();
+            KosciImages.Add(new BitmapImage(new Uri("grafika/pusta.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc1.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc2.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc3.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc4.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc5.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc6.png", UriKind.Relative)));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Random random = new Random();
+            List<int> wylosowane = new List<int>();
+            wylosowane.Clear();
+            for(int i=0;i<5;i++)
+                wylosowane.Add(random.Next(1, 7));
+            k1.Source = KosciImages[wylosowane[0]];
+            k2.Source = KosciImages[wylosowane[1]];
+            k3.Source = KosciImages[wylosowane[2]];
+            k4.Source = KosciImages[wylosowane[3]];
+            k5.Source = KosciImages[wylosowane[4]];
         }
     }
 }
