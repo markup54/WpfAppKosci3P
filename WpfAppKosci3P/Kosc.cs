dde407fa650ffd7bf454c51ed8b813cd45f3b585
markup54 <marzenakupka@tlen.pl>
2024-01-05 11:24:37 +0100

    dziala rzucanie i blokowanie

---
+++ WpfAppKosci3P/Kosc.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace WpfAppKosci3P
+{
+    public class Kosc
+    {
+        public int Wartosc { get; set; }
+        public BitmapImage Obraz { get; set; }
+        public bool CzyZablokowana { get; set; }
+        public List <BitmapImage> KosciImages { get; set; }
+
+        public Kosc()
+        {
+            Wartosc = 0;
+            przygotujKosc();
+            Obraz = KosciImages[Wartosc];
+            CzyZablokowana = false;
+        }
+        private void przygotujKosc()
+        {
+            KosciImages = new List<BitmapImage>();
+            KosciImages.Add(new BitmapImage(new Uri("grafika/pusta.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc1.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc2.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc3.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc4.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc5.png", UriKind.Relative)));
+            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc6.png", UriKind.Relative)));
+        }
+        public void losowanie()
+        {
+            Random random = new Random();
+            if (CzyZablokowana == false)
+            {
+                Wartosc = random.Next(1,7);
+            }
+            Obraz = KosciImages[Wartosc];
+        }
+        public void zablokuj()
+        {
+            CzyZablokowana = true;  
+        }
+    }
+}
--- WpfAppKosci3P/MainWindow.xaml.cs
+++ WpfAppKosci3P/MainWindow.xaml.cs
@@ -20,40 +20,50 @@ namespace WpfAppKosci3P
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<BitmapImage> KosciImages { get; set; }
+        public List<Kosc> Kosci { get; set; }
+        public List<Image> Images { get; set; }
         public MainWindow()
         {
             InitializeComponent();
             przygotujKosci();
+            przygotujObrazki();
         }
         private void przygotujKosci()
         {
-            KosciImages = new List<BitmapImage>();
-            KosciImages.Add(new BitmapImage(new Uri("grafika/pusta.png", UriKind.Relative)));
-            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc1.png", UriKind.Relative)));
-            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc2.png", UriKind.Relative)));
-            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc3.png", UriKind.Relative)));
-            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc4.png", UriKind.Relative)));
-            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc5.png", UriKind.Relative)));
-            KosciImages.Add(new BitmapImage(new Uri("grafika/kosc6.png", UriKind.Relative)));
+            Kosci = new List<Kosc>();
+            Kosci.Add(new Kosc());
+            Kosci.Add(new Kosc());
+            Kosci.Add(new Kosc());
+            Kosci.Add(new Kosc());
+            Kosci.Add(new Kosc());
+        }
+
+        private void przygotujObrazki()
+        {
+            Images = new List<Image>();
+            Images.Add(k1);
+            Images.Add(k2);
+            Images.Add(k3);
+            Images.Add(k4);
+            Images.Add(k5);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Random random = new Random();
-            List<int> wylosowane = new List<int>();
-            wylosowane.Clear();
-            for(int i=0;i<5;i++)
-                wylosowane.Add(random.Next(1, 7));
-            k1.Source = KosciImages[wylosowane[0]];
-            k2.Source = KosciImages[wylosowane[1]];
-            k3.Source = KosciImages[wylosowane[2]];
-            k4.Source = KosciImages[wylosowane[3]];
-            k5.Source = KosciImages[wylosowane[4]];
+           for(int i = 0; i < Kosci.Count; i++)
+            {
+                Kosci[i].losowanie();
+                Images[i].Source = Kosci[i].Obraz;
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
+            Button button = sender as Button;
+            Image image = button.Content as Image;
+            int ktory = Images.IndexOf(image);
+            Kosci[ktory].zablokuj();
+            image.Opacity = 0.3;
 
         }
     }
